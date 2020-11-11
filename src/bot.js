a6f588bc01967fdc1668bcf133a9d392b9d15c07
osidekyle <hawkinskyle67@gmail.com>
2020-11-10 22:24:15 -0800

    differnet bot commands, text commands, permission

--- src/bot.js
+++ src/bot.js
@@ -4,6 +4,7 @@ const {Client}=require("discord.js")
 
 
 const client=new Client();
+const PREFIX="$";   
 
 client.on('ready', ()=>{
     console.log(`${client.user.username} has logged in`)
@@ -11,9 +12,30 @@ client.on('ready', ()=>{
 
 
 client.on('message',(message)=>{
+    if(message.author.bot) return
+    if(message.content.startsWith(PREFIX)){
+        const [CMD_NAME,...args]=message.content
+        .trim()
+        .substring(PREFIX.length)
+        .split(/\s+/)
+       if(CMD_NAME==="kick"){
+           if(message.member.hasPermission)
+           if(args.length===0) return message.reply("Please provide an ID")
+           const member = message.guild.members.cache.get(args[0]);
+           if(member){
+               member.kick()
+               .then((member)=>message.channel.send(`${member} was kicked`))
+               .catch((err)=>message.channel.send("I do not have permission"))
+           }
+           else{
+               message.channel.send("Member not found")
+           }
+       }
+       
+    }
     console.log(`${message.author.tag} has sent the message "${message.content}"`)
     if(message.content==="hello"){
-        message.reply("hello there!")
+        message.channel.send("Hello")
     }
 })
 
